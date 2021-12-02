5396de746f5631b1be96542e873d54546b79f5ac
umorigu <umorigu@gmail.com>
2021-12-02 23:09:28 +0900

    PukiWiki 1.5.2 and PHP 5.2
    * Support Simple URL (get_page_uri(); PukiWiki 1.5.2+))
    * Use CP51932 to support platform dependent characters (PHP5.2+)
    * Support PKWK_SAFE_MODE and PKWK_READONLY
    * Improve HTTP status 302 message (Found)
    * New constant: PLUGIN_S_VIRTUAL_QUERY_PREFIX
      * Delete: PLUGIN_S_COMMAND_STR
    * License: GPL v2
    * Add README.md

---
+++ README.md
@@ -0,0 +1,87 @@
+# PukiWiki URL短縮プラグイン s.inc.php
+
+PukiWiki URL短縮プラグイン (s.inc.php) は短いURLによってページへのアクセスを可能にするプラグインです。
+
+## 使い方
+
+https://pukiwiki.osdn.jp/?UserPlugins/s.inc.php
+
+## セットアップ
+
+### s.inc.php 配置
+
+* https://github.com/umorigu/pukiwiki.urlshortener から s.inc.php をダウンロードする。
+* s.inc.php を plugin/ ディレクトリに配置する。
+
+### ディレクトリ作成
+
+wiki/ や plugin/ と並列の位置に次の2つのディレクトリを作成する。
+
+* shortener/
+* shortener_counter/
+
+パーミッションの設定
+
+```
+chmod -R 777 shortener shortener_counter
+```
+
+### skin/pukiwiki.skin.php を編集
+
+```
+<?php if ($is_page) { ?>
+ <?php if(SKIN_DEFAULT_DISABLE_TOPICPATH) { ?>
+   <a href="<?php echo $link['reload'] ?>"><span class="small"><?php echo $link['reload'] ?></span></a>
+ <?php } else { ?>
+   <span class="small">
+   <?php require_once(PLUGIN_DIR . 'topicpath.inc.php'); echo plugin_topicpath_inline(); ?>
+   </span>
+ <?php } ?>
+<?php } ?>
+```
+
+ここ↑の置き換え、または下あたりに↓を追加する。
+
+```
+<?php if ($is_page) { ?>
+ <br>
+ <span class="small">
+ <?php require_once(PLUGIN_DIR . 's.inc.php'); echo plugin_s_convert_get_short_link(); ?>
+ </span>
+<?php } ?>
+```
+
+実際はどこか1箇所に書けばよい。
+
+
+### mod_rewriteを併用する場合
+
+mod_rewrite用の.htaccessの例
+
+```
+RewriteEngine on
+RewriteBase /var/www/html/pukiwiki
+RewriteCond %{QUERY_STRING} ^&([0-9a-f]+)$
+RewriteRule ^(.*)$ /pukiwiki/?cmd=s&k=%1
+```
+
+これで、
+
+http://w.example.com/pukiwiki/?&2dc9e012b6
+
+のような /?&xxxxxxxxxx 形式でアクセスできるようになる。
+
+## 設定値
+
+s.inc.php で設定する。
+
+* define('PLUGIN_S_PAGEID_LENGTH', 10);
+  * ページを表すキーを何文字にするか。最大32文字
+* define('PLUGIN_S_COMMAND_STR', 'cmd=s&k=');
+  * "/?" と キーの間の文字列。mod_rewrite が使える場合は、ここで '&' などを指定する。
+* define('PLUGIN_S_PAGENAME_MININUM_LENGTH', 20);
+  * 「ここの数字より短いページ名URLの場合には短縮URLを表示しない」
+
+## Licenses
+
+GPL v2
--- src/s.inc.php
+++ src/s.inc.php
@@ -1,8 +1,9 @@
 <?php
 // s.inc.php
 //
-// URL Shortener plugin
-// License: The same as PukiWiki
+// Copyright 2011-2021 umorigu
+// PukiWiki URL Shortener plugin
+// License: GPLv2
 
 // key-name map directory
 define('PLUGIN_S_NAMES_DIR', 'shortener');
@@ -13,20 +14,22 @@ define('PLUGIN_S_NAMES_COUNTER_DIR', 'shortener_counter');
 // key string length
 define('PLUGIN_S_PAGEID_LENGTH', 10);
 
-// command string "/?${PLUGIN_S_COMMAND_STR}91aa88d26e";  
-define('PLUGIN_S_COMMAND_STR', 'cmd=s&k=');
+// virtual query prefix "/${PLUGIN_S_VIRTUAL_QUERY_PREFIX}91aa88d26e";
+define('PLUGIN_S_VIRTUAL_QUERY_PREFIX', '?cmd=s&k=');
 
 // page name minimum length   
 define('PLUGIN_S_PAGENAME_MININUM_LENGTH', 20);
 
+// use short url if original url contains percent chars
+define('PLUGIN_S_SHORT_URL_ON_PERCENT', FALSE);
+
 function plugin_s_convert_get_short_link()
 {
 	$shorturl = plugin_s_inline_get_short_url();
-	if ($shorturl === '')
-	{
+	if (! $shorturl) {
 		return '';
 	}
-	$s = htmlspecialchars($shorturl);
+	$s = htmlsc($shorturl);
 	$jsblock = <<<JS_BLOCK_END
 <script type="text/javascript">
 var __plugin_s_hash = '';
@@ -65,71 +68,127 @@ function plugin_s_inline_get_short_url()
 {
 	global $vars;
 	$page = $vars['page'];
-	if (is_page($page) &&
-		PLUGIN_S_PAGENAME_MININUM_LENGTH < strlen(rawurlencode($page)))
-	{
-		$utf8page = $page; 
-		if (! defined('PKWK_UTF8_ENABLE'))
-		{
-			$utf8page = mb_convert_encoding(mb_convert_encoding($page, 'SJIS-win', 'EUC-JP'), 'UTF-8', 'SJIS-win');
-		}
-		$encoded = encode($utf8page);
-		$md5 = md5($encoded);
-		$shortid = substr($md5, 0, PLUGIN_S_PAGEID_LENGTH);
-		$shorturl = get_script_uri() . '?' . PLUGIN_S_COMMAND_STR . $shortid;
-		$filename = PLUGIN_S_NAMES_DIR . '/' . $shortid . '.txt';
-		if (!file_exists($filename))
-		{
-			$fp = fopen($filename, 'w') or die('fopen() failed: ' . $filename);
-			set_file_buffer($fp, 0);
-			flock($fp, LOCK_EX);
-			rewind($fp);
-			fputs($fp, $utf8page);
-			flock($fp, LOCK_UN);
-			fclose($fp);
-		}
-		return $shorturl;
-	}
-	return '';
+	return plugin_s_get_short_url_from_page($page);
 }
 
-// Action-type plugin: ?plugin=s&k=key
+/**
+ * Get short absolute URL (with http(s):// scheme) for existing page.
+ *
+ * @return short or original url for existing page
+ * @return '' if the page doesn't exists
+ */
+function plugin_s_get_short_url_from_page($page)
+{
+	$page_id = plugin_s_get_page_id($page);
+	if ($page_id === FALSE) {
+		// $page doesn't exists
+		return '';
+	}
+	if (! $page_id) {
+		return get_page_uri($page, PKWK_URI_ABSOLUTE);
+	}
+	$shorturl = get_base_uri(PKWK_URI_ABSOLUTE)
+		. PLUGIN_S_VIRTUAL_QUERY_PREFIX . $page_id;
+	return $shorturl;
+}
+
+/**
+ * Create and get page_id of existing page.
+ *
+ * @param $page page name
+ * @return FALSE: page doesn't exist
+ * @return null: page_id should not be created (page name is enouth short. etc.)
+ * @return page_id: 10 hex chars that indicate existing page
+ */
+function plugin_s_get_page_id($page)
+{
+	if (! is_page($page)) {
+		return FALSE;
+	}
+	$utf8page = $page;
+	if (! defined('PKWK_UTF8_ENABLE')) {
+		$utf8page = mb_convert_encoding($page, 'UTF-8', 'CP51932');
+	}
+	$page_encoded = pagename_urlencode($utf8page);
+	if (strlen($page_encoded) <= PLUGIN_S_PAGENAME_MININUM_LENGTH) {
+		if (PLUGIN_S_SHORT_URL_ON_PERCENT) {
+			if (strpos($page_encoded, '%') === FALSE) {
+				return null;
+			}
+			// page_id is available when original url contains '%'.
+		} else {
+			return null;
+		}
+	}
+	$encoded = encode($utf8page);
+	$md5 = md5($encoded);
+	$page_id = substr($md5, 0, PLUGIN_S_PAGEID_LENGTH);
+
+	// Create page_id to page_name map file in shortener/ dir
+	$filename = PLUGIN_S_NAMES_DIR . '/' . $page_id . '.txt';
+	if (! file_exists($filename)) {
+		$fp = fopen($filename, 'w') or die('fopen() failed: ' . htmlsc($filename));
+		set_file_buffer($fp, 0);
+		flock($fp, LOCK_EX);
+		rewind($fp);
+		fputs($fp, $utf8page);
+		flock($fp, LOCK_UN);
+		fclose($fp);
+	}
+	return $page_id;
+}
+/**
+ * Action-type plugin: ?plugin=s&k={page_id}.
+ */
 function plugin_s_action()
 {
 	global $vars;
-	
-	// See above
-	if (PKWK_SAFE_MODE || PKWK_READONLY)
-		die_message('PKWK_SAFE_MODE or PKWK_READONLY prohibits this');
+	$page_id = isset($vars['k']) ? $vars['k'] : '';
+	$page = plugin_s_get_page_from_page_id($page_id);
+	if (! $page) {
+		die_message("Invalid page id: " . htmlsc($page_id));
+		exit;
+	}
+	$url = get_page_uri($page, PKWK_URI_ROOT);
+	header("HTTP/1.1 302 Found");
+	header("Location: $url");
+	exit;
+}
 
-	$pageid = isset($vars['k']) ? $vars['k'] : '';
-	
-	$filename = PLUGIN_S_NAMES_DIR . '/' . $pageid . '.txt';
-	$fp = fopen($filename, 'r') or die_message('Cannnot open ' . $filename);
-	$str = fgets($fp);
-	$str2 = trim($str);
+/**
+ * Get page name from page_id.
+ *
+ * @param $page_id page id - 1-32 hex chars
+ * @return page name
+ * @return FALSE if page_id is invalid or map file is not found
+ */
+function plugin_s_get_page_from_page_id($page_id)
+{
+	if (! preg_match('#^[a-f0-9]{10,32}$#', $page_id)) {
+		return FALSE;
+	}
+	$filename = PLUGIN_S_NAMES_DIR . '/' . $page_id . '.txt';
+	$fp = fopen($filename, 'r');
+	if (! $fp) {
+		return FALSE;
+	}
+	$page = trim(fgets($fp));
+	if (! defined('PKWK_UTF8_ENABLE')) {
+		$page = mb_convert_encoding($page, 'CP51932', 'UTF-8');
+	}
 	fclose($fp);
-	
+
 	// increment counter
-	$cfilename = PLUGIN_S_NAMES_COUNTER_DIR . '/' . $pageid . '.count';
+	$cfilename = PLUGIN_S_NAMES_COUNTER_DIR . '/' . $page_id . '.count';
 	$fpc = fopen($cfilename, file_exists($cfilename) ? 'r+' : 'w+')
-		or die_message('Cannot open: ' . $cfilename);
+		or die_message('Cannot open: ' . htmlsc($cfilename));
 	set_file_buffer($fpc, 0);
 	flock($fpc, LOCK_EX);
-	$shorter_count = trim(fgets($fpc));
-	$shorter_count = intval($shorter_count) + 1;
+	$shorter_count = intval(trim(fgets($fpc))) + 1;
 	rewind($fpc);
 	fputs($fpc, $shorter_count);
 	flock($fpc, LOCK_UN);
 	fclose($fpc);
-	
-	if (!defined('PKWK_UTF8_ENABLE'))
-	{
-		$str2 = mb_convert_encoding(mb_convert_encoding($str2, 'SJIS-win', 'UTF-8'), 'EUC-JP', 'SJIS-win');
-	}
-	$url = get_script_uri() . '?' . rawurlencode($str2);
-	header("HTTP/1.1 302 Moved Permanently");
-	header("Location: $url");
-	exit;
+
+	return $page;
 }
-?>
